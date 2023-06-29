06b3cc765c30a924cca1b1a519c665623131663c
koo-lab <koolaboratory.gmail.com>
2023-06-29 14:44:27 -0400

    updates

--- data/download.sh
+++ data/download.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# wget gencode
 # wget hg19.fa
+wget https://www.dropbox.com/s/mojkzvlmsw2bk6b/hg19.fa
 
+# wget gencode
+wget https://www.dropbox.com/s/1j8imw5eykjyzvm/gencode.v43lift37.basic.annotation.gtf.gz
+gunzip gencode.v43lift37.basic.annotation.gtf.gz
 
