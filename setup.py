6463884b03c9c286268a09a71361db47d461edba
koo-lab <koolaboratory.gmail.com>
2023-07-10 23:33:55 -0400

    updates

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="creme-nn",
     # TODO: Consider using https://github.com/python-versioneer/python-versioneer to
     # get version information from git.
-    version="0.1.1",
+    version="0.1.2",
     description="An in silico perturbation framework to interpret large-scale genomic deep learning",
     # author='A. Random Developer',
     # author_email='author@example.com',
