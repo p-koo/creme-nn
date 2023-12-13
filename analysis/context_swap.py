d00905e0aa3c464d052780341121555e460c23e7
shtoneyan <tikinbobo@gmail.com>
2023-12-13 06:52:49 -0500

    fix context folder path

--- analysis/context_swap.py
+++ analysis/context_swap.py
@@ -9,6 +9,7 @@ import sys, os, h5py
 import kipoiseq
 from tqdm import tqdm
 import json
+
 sys.path.append('../creme')
 import creme
 import custom_model
@@ -16,12 +17,7 @@ import utils
 import glob
 
 
-
-
-
-
 def main():
-
     model_name = sys.argv[1]
     data_dir = '../data/'
     result_dir = f'../results/'
@@ -48,37 +44,28 @@ def main():
     results_dir = utils.make_dir(f'{result_dir}/context_swap_test/')
     test_results_dir = utils.make_dir(f'{results_dir}/{model_name}/')
 
-    dfs = {cell_line: pd.read_csv(f'../results/context_dependence_test/{model_name}/{cell_line}_context.csv') for cell_line in cell_lines}
+    dfs = {cell_line: pd.read_csv(f'../results/context_dependence_test_100/{model_name}/{cell_line}_context.csv') for
+           cell_line in cell_lines}
 
     for cell, df in dfs.items():
+        df = df.sample(frac=1)
         cell_line_dir = utils.make_dir(f'{test_results_dir}/{cell}')
-        x_target = []
-        l_target = []
+        sequences = {}
         for i, row in tqdm(df.iterrows()):
             # get sequence from reference genome and convert to one-hot
-            target_seq_info = row['path'].split('/')[-1].split('.')[0]
-            chrom, start, strand = target_seq_info.split('_')[1:]
-            l_target.append(target_seq_info)
-            x_target.append(seq_parser.extract_seq_centered(chrom, int(start), strand, model.seq_length, onehot=True))
-        x_target = np.array(x_target)
+            seq_info = row['path'].split('/')[-1].split('.')[0]
+            chrom, start, strand = seq_info.split('_')[1:]
+            sequences[seq_info] = seq_parser.extract_seq_centered(chrom, int(start), strand, model.seq_length,
+                                                                  onehot=True)
 
-        df = df.sample(frac=1)
-        for i, row in tqdm(df.iterrows(), total=df.shape[0]):
-            src_seq_info = row['path'].split('/')[-1].split('.')[0]
-            result_path = f'{cell_line_dir}/{src_seq_info}.pickle'
+        for src_seq_info, src_seq in tqdm(sequences.items(), total=len(sequences.keys())):
+            for dest_seq_info, dest_seq in sequences.items():
 
-            if not os.path.isdir(result_path):
+                result_path = f'{cell_line_dir}/src_{src_seq_info}_dest_{dest_seq_info}.pickle'
 
-                chrom, start, strand = src_seq_info.split('_')[1:]
-
-                x_source = seq_parser.extract_seq_centered(chrom, int(start), strand, model.seq_length, onehot=True)
-
-                pred_mut = creme.context_swap_test(model, x_source, x_target, l_target, tss_tile)
-
-                utils.save_pickle(result_path, pred_mut)
-
-
-
+                if not os.path.isdir(result_path):
+                    pred_mut = creme.context_swap_test(model, src_seq, dest_seq, tss_tile)
+                    utils.save_pickle(result_path, pred_mut)
 
 
 if __name__ == '__main__':
