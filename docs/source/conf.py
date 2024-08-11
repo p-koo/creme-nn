2a89d00934f1e1ae717adaa7da22496fd37b2306
PK <koo@cshl.edu>
2024-08-10 21:46:07 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,7 +3,9 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
 import sys
+sys.path.insert(0, os.path.abspath('../..'))
 sys.path.append("../../creme/")
 import creme
 
